2fc33ee01c7155adc98ab7da7e3a2336bb5191be
Rachel <rachel.tucker.rt@gmail.com>
2023-04-09 21:29:39 +1000

    UI addition
    Adding in user interface

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -4,7 +4,81 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Placeholder");
+            bool shouldContinueBanking = true;
+            bool shouldContinueAsking = true;
+            Console.WriteLine("Welcome to ISE Banking App.");
+            while (shouldContinueBanking)
+            {
+                Console.WriteLine("\nPlease enter the number code of the operation to perform.");
+                Console.WriteLine("\t1. Log in");
+                Console.WriteLine("\t2. Sign up");
+                string? choice = Console.ReadLine();
+                choice = choice?.Trim().ToLower() ?? "";
+
+                switch (choice)
+                {
+                    case "1":
+                    case "l":
+                    case "log in":
+                    case "login":
+                        Login();
+                        break;
+
+                    case "2":
+                    case "s":
+                    case "sign up":
+                    case "signup":
+                        Signup();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        continue;
+                }
+
+                while (shouldContinueAsking)
+                {
+                    Console.WriteLine("\nWould you like to continue banking?");
+                    Console.WriteLine("\t1. Yes\n\t2. No");
+                    choice = Console.ReadLine();
+                    choice = choice?.Trim().ToLower() ?? "";
+
+                    switch (choice)
+                    {
+                        case "1":
+                        case "y":
+                        case "yes":
+                            shouldContinueAsking = false;
+                            continue;
+
+                        case "2":
+                        case "n":
+                        case "no":
+                            shouldContinueBanking = false;
+                            shouldContinueAsking = false;
+                            continue;
+
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
+                }
+                shouldContinueAsking = true;
+            }
+
+            Console.WriteLine("\nThank you for banking with ISE Bank.");
+            Console.ReadKey();
+            return;
+        }
+
+        public static void Login()
+        {
+            Console.WriteLine("\nPlaceholder");
+        }
+
+        public static void Signup()
+        {
+            Console.WriteLine("\nPlaceholder");
         }
     }
 
