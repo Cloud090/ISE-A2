d677ea02ff593b9e513423677bb856359918f1f5
Rachel <rachel.tucker.rt@gmail.com>
2023-04-10 09:27:45 +1000

    Various changes
    Separated classes into new files and changed some constructors.
    
    Still missing working sign up and log in modules.

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace BankAccount
+﻿namespace BankApp
 {
     internal class Program
     {
@@ -13,7 +13,7 @@
                 Console.WriteLine("\t1. Log in");
                 Console.WriteLine("\t2. Sign up");
                 string? choice = Console.ReadLine();
-                choice = choice?.Trim().ToLower() ?? "";
+                choice = choice?.Trim().ToLower() ?? string.Empty;
 
                 switch (choice)
                 {
@@ -21,14 +21,14 @@
                     case "l":
                     case "log in":
                     case "login":
-                        Login();
+                        Console.WriteLine("Log in placeholder");
                         break;
 
                     case "2":
                     case "s":
                     case "sign up":
                     case "signup":
-                        Signup();
+                        Console.WriteLine("Sign up placeholder");
                         break;
 
                     default:
@@ -36,12 +36,12 @@
                         continue;
                 }
 
-                while (shouldContinueAsking)
+                do
                 {
                     Console.WriteLine("\nWould you like to continue banking?");
                     Console.WriteLine("\t1. Yes\n\t2. No");
                     choice = Console.ReadLine();
-                    choice = choice?.Trim().ToLower() ?? "";
+                    choice = choice?.Trim().ToLower() ?? string.Empty;
 
                     switch (choice)
                     {
@@ -60,108 +60,16 @@
 
                         default:
                             Console.WriteLine("Invalid choice. Please try again.");
+                            shouldContinueAsking = true;
                             break;
                     }
                 }
-                shouldContinueAsking = true;
+                while (shouldContinueAsking);
             }
 
             Console.WriteLine("\nThank you for banking with ISE Bank.");
             Console.ReadKey();
             return;
-        }
-
-        public static void Login()
-        {
-            Console.WriteLine("\nPlaceholder");
-        }
-
-        public static void Signup()
-        {
-            Console.WriteLine("\nPlaceholder");
-        }
-    }
-
-    public class BankAccount
-    {
-        private static int accountNumberSeed = 147258369;
-        public string NumberID { get; }
-        public string Owner { get; set; }
-        public decimal Balance
-        {
-            get
-            {
-                decimal balance = 0;
-                foreach (var item in allTransactions)
-                {
-                    balance += item.Amount;
-                }
-                return balance;
-            }
-        }
-
-        public BankAccount(string name, decimal initialBalance)
-        {
-            this.NumberID = accountNumberSeed.ToString();
-            accountNumberSeed++;
-
-            this.Owner = name;
-            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
-        }
-
-        public void MakeDeposit(decimal amount, DateTime date, string note)
-        {
-            if (amount <= 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(amount), "Deposit amount must be positive");
-            }
-            var deposit = new Transaction(amount, date, note);
-            allTransactions.Add(deposit);
-        }
-
-        public void MakeWithdrawal(decimal amount, DateTime date, string note)
-        {
-            if (amount <= 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(amount), "Withdrawal amount must be positive");
-            }
-
-            if (Balance - amount < 0)
-            {
-                throw new InvalidOperationException("Insufficient funds for requested withdrawal");
-            }
-            var withdrawal = new Transaction(-amount, date, note);
-            allTransactions.Add(withdrawal);
-        }
-
-        private List<Transaction> allTransactions = new List<Transaction>();
-
-        public string GetAccountHistory()
-        {
-            var report = new System.Text.StringBuilder();
-
-            decimal balance = 0;
-            report.AppendLine("Date\t\tAmount\tBalance\tNote");
-            foreach (var item in allTransactions)
-            {
-                balance += item.Amount;
-                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Note}");
-            }
-            return report.ToString();
-        }
-    }
-
-    public class Transaction
-    {
-        public decimal Amount { get; }
-        public DateTime Date { get; }
-        public string Note { get; }
-
-        public Transaction(decimal amount, DateTime date, string note)
-        {
-            this.Amount = amount;
-            this.Date = date;
-            this.Note = note;
         }
     }
 }
---
+++ BankAccount/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    public class Transaction
+    {
+        public decimal Amount { get; init; }
+        public DateTime Date { get; init; }
+        public string Note { get; init; } = string.Empty;
+    }
+}
