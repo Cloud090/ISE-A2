ec79e86b463fbdb44636643fe204916073729a0c
cloud090 <james.brown@brownbrothers.net.au>
2023-04-11 10:06:33 +1000

    Signup in its own file - some program.cs changes

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Mail;
+using static BankApp.Signup; 
 
 namespace BankApp
 {
@@ -7,17 +8,22 @@ namespace BankApp
         static void Main(string[] args)
         {
             bool shouldContinueBanking = true;
-            bool shouldContinueAsking = true;
+            //bool shouldContinueAsking = true;
             Console.WriteLine("Welcome to ISE Banking App.");
+
+            List<User> users = new List<User>();
+
             while (shouldContinueBanking)
             {
                 Console.WriteLine("\nPlease enter the number code of the operation to perform.");
                 Console.WriteLine("\t1. Log in");
                 Console.WriteLine("\t2. Sign up");
+                Console.WriteLine("\t3. Exit");
                 string choice = Console.ReadLine()!;
                 choice = choice?.Trim().ToLower() ?? string.Empty;
 
-                List<User> users = new List<User>();
+                
+                
 
                 switch (choice)
                 {
@@ -25,104 +31,33 @@ namespace BankApp
                     case "l":
                     case "log in":
                     case "login":
-                        Console.WriteLine("Log in placeholder");
+                        Login(users);
                         break;
 
                     case "2":
                     case "s":
                     case "sign up":
                     case "signup":
-                        Register(users);
+                        Signup.Register(users);
+                        break;
+                    case "3":
+                    case "Exit":
+                    case "exit":
+                        shouldContinueBanking = false;
                         break;
                     default:
                         Console.WriteLine("Invalid choice. Please try again.");
                         continue;
                 }
 
-                do
-                {
-                    Console.WriteLine("\nWould you like to continue banking?");
-                    Console.WriteLine("\t1. Yes\n\t2. No");
-                    choice = Console.ReadLine()!;
-                    choice = choice?.Trim().ToLower() ?? string.Empty;
-
-                    switch (choice)
-                    {
-                        case "1":
-                        case "y":
-                        case "yes":
-                            shouldContinueAsking = false;
-                            continue;
-
-                        case "2":
-                        case "n":
-                        case "no":
-                            shouldContinueBanking = false;
-                            shouldContinueAsking = false;
-                            continue;
-
-                        default:
-                            Console.WriteLine("Invalid choice. Please try again.");
-                            shouldContinueAsking = true;
-                            break;
-                    }
-                }
-                while (shouldContinueAsking);
+                
             }
 
             Console.WriteLine("\nThank you for banking with ISE Bank.");
             Console.ReadKey();
             return;
         }
-        class User
-        {
-            public string Email { get; set; }
-            public string Password { get; set; }
-        } 
-        static void Register(List<User> users)
-        {
-            Console.WriteLine("Enter your email:");
-            string email = Console.ReadLine()!;
-
-            
-
-            while (email == "" || IsValid(email) == false){
-                Console.WriteLine("Invalid Email. Try again.");
-                Console.WriteLine("Enter Email:");
-                email = Console.ReadLine()!;
-            }
-
-            Console.WriteLine("Create a password:");
-            string password = Console.ReadLine()!;
-
-            if (password == ""){
-                Console.WriteLine("Invalid Password. Try again.");
-                Console.WriteLine("Create a password:");
-                password = Console.ReadLine()!;
-            }
-
-            User newUser = new User { Email = email, Password = password };
-            users.Add(newUser);
-
-            Console.WriteLine("Registration successful!");
-            Console.WriteLine("Email is: " + email);
-        }  
-
-         private static bool IsValid(string email)
-        { 
-            var valid = true;
-            
-            try
-            { 
-                var emailAddress = new MailAddress(email);
-            }
-            catch
-            {
-                valid = false;
-            }
-
-            return valid;
-        }
+        
         
     }
 }
--- BankAccount/Signup.cs
+++ BankAccount/Signup.cs
@@ -1,14 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BankApp;
+﻿using System.Net.Mail;
 
 namespace BankApp
 {
+    public class User
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    } 
     public class Signup
     {
         
+        public static void Register(List<User> users)
+        {
+            int registerAttemps = 0;
+            Console.WriteLine("Enter your email:");
+            string email = Console.ReadLine()!;
+            registerAttemps++;
+            
+
+            while (email == "" && registerAttemps <= 3 || IsValid(email) == false && registerAttemps <= 3){
+                Console.WriteLine("Invalid Email. Try again.");
+                Console.WriteLine("Enter Email:");
+                email = Console.ReadLine()!;
+                registerAttemps++;
+            }
+
+            Console.WriteLine("Create a password:");
+            string password = Console.ReadLine()!;
+            registerAttemps++;
+            while (password == "" && registerAttemps <= 3 ){
+                Console.WriteLine("Invalid Password. Try again.");
+                Console.WriteLine("Create a password:");
+                password = Console.ReadLine()!;
+            }
+            if (registerAttemps > 3) {
+                Console.WriteLine("Registration Unsucessful. Try again later.");
+            }
+            else {
+                User newUser = new User { Email = email, Password = password };
+                users.Add(newUser);
+
+                Console.WriteLine("Registration successful!");
+                Console.WriteLine("\nEmail is: " + email + "\n Password: Set");
+            }
+            
+        }
+
+         private static bool IsValid(string email)
+        { 
+            var valid = true;
+            
+            try
+            { 
+                var emailAddress = new MailAddress(email);
+            }
+            catch
+            {
+                valid = false;
+            }
+
+            return valid;
+        }
     }
 }
