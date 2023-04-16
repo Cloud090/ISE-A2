74d9df2e2cf23dc50d575fdbf055dc442d4c1f11
unknown <james.brown@brownbrothers.net.au>
2023-04-16 18:14:26 +1000

    case redundancy redundancy

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -31,49 +31,6 @@ namespace BankApp
                     HandleBankServices(ref currentSessionState, user);
                 }
             }
-            bool shouldContinueBanking = true;
-            //bool shouldContinueAsking = true;
-            Console.WriteLine("Welcome to ISE Banking App.");
-
-            
-
-            while (shouldContinueBanking)
-            {
-                Console.WriteLine("\nPlease enter the number code of the operation to perform.");
-                Console.WriteLine("\t1. Log in");
-                Console.WriteLine("\t2. Sign up");
-                Console.WriteLine("\t3. Exit");
-                string choice = Console.ReadLine()!;
-                choice = choice?.Trim().ToLower() ?? string.Empty;
-
-                switch (choice)
-                {
-                    case "1":
-                    case "l":
-                    case "log in":
-                    case "login":
-                        Login.Authenticate(users);
-                        break;
-
-                    case "2":
-                    case "s":
-                    case "sign up":
-                    case "signup":
-                        Signup.Register(users);
-                        break;
-                    case "3":
-                    case "Exit":
-                    case "exit":
-                        shouldContinueBanking = false;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice. Please try again.");
-                        continue;
-                }
-
-                
-
-            }
 
             Console.WriteLine("\nThank you for banking with ISE Bank.");
             Console.ReadKey();
