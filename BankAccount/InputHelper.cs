6a24fea39114dc3ab2f7c4e47fe82f2dfc780bc8
unknown <james.brown@brownbrothers.net.au>
2023-05-09 16:17:07 +1000

    comments removed

--- BankAccount/InputHelper.cs
+++ BankAccount/InputHelper.cs
@@ -3,11 +3,11 @@ namespace BankApp
 { 
     public static class InputHelper
     {
-        public static bool IsEscapeWord(string input) // Checks if input is escape word to return to main menu 
+        public static bool IsEscapeWord(string input)
         {
-            string escapeWord = "exit"; // Input that triggers return to main menu
+            string escapeWord = "exit";
             if (input == null) { return false; }
-            return input.Equals(escapeWord, StringComparison.OrdinalIgnoreCase); // Compares input to escape word, ignoring case, and returns Boolean
+            return input.Equals(escapeWord, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
